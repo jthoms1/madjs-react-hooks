7af06e29501dceab278fe95984c29a61777fd1fa
Josh Thomas <jthoms1@gmail.com>
2018-12-09 15:19:13 -0600

    add usestate example

---
+++ examples/src/components/useState/ExampleOneOld.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+type State = {
+  count: number
+}
+
+export default class Counter extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      count: 0
+    }
+  }
+
+  setCount(newCount: number) {
+    this.setState((prevState) => ({
+      count: newCount
+    }))
+  }
+
+  render() {
+    return (
+      <div className="counter">
+        <p>You clicked {this.state.count} times.</p>
+        <button onClick={() => this.setCount(this.state.count + 1)}>Click me</button>
+      </div>
+    )
+  }
+};
